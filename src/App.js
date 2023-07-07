25d570e6511c503e9c415d99957256ee75044e7c
Leo Božić <leobozic2903@gmail.com>
2023-07-07 19:06:51 +0200

    Dodan random string

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import randomstring from "randomstring";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <h2>{randomstring.generate()}</h2>
       </header>
     </div>
   );
